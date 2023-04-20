59c9abbe19dc4cfd80b519c0ffc7a3042f222d12
ashura <ghanatva@gmail.com>
2023-04-20 19:46:52 +0530

    tables added

---
+++ proposals/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from  users.models import Users
+
+# Create your models here.
+class talks(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.TextField(max_length=400)
+    domain=models.CharField(max_length=50)
+    slides=models.BooleanField(verbose_name="Yes",default=False)
+    username=models.ForeignKey(Users,on_delete=models.CASCADE)
+    duration=models.DurationField()
+    availability=models.DateField()
+
+    def __str__(self):
+        return self.title
