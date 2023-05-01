611a00a88cf42adb991d660ce4d7c954177faf35
ashura <ghanatva@gmail.com>
2023-05-01 20:10:36 +0530

    test postgres

--- cfp_portal/settings.py
+++ cfp_portal/settings.py
@@ -81,10 +81,10 @@ import os
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql',
-    'HOST': os.environ.get('DB_HOST'),
-    'NAME': os.environ.get('DB_NAME'),
-    'USER': os.environ.get('DB_USER'),
-    'PASSWORD': os.environ.get('DB_PASS'),
+    'HOST': 'localhost',
+    'NAME': 'cfp_portal',
+    'USER': 'postgres',
+    'PASSWORD': 'postgres',
   }
 }
 
