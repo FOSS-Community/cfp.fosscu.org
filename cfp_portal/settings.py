72fe23bcb819a4f0e4b0fecb32e56584d5aa3849
Sam <asamadans@gmail.com>
2023-05-10 00:54:18 +0530

    add proposals app to settings

--- cfp_portal/settings.py
+++ cfp_portal/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "users.apps.UsersConfig",
+    "proposals.apps.Proposals.Config",
     "compressor",
     "django.contrib.admin",
     "django.contrib.auth",
