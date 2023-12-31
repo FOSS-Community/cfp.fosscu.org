c799baaa0e224c774432901c089666068e37ad6d
Sam <asamadans@gmail.com>
2023-12-31 13:24:05 +0530

    fixed login authentication

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.Login, name='login'),
+    path("", views.login_view, name='login_view'),
     path("sign_up/", views.sign_up, name='sign_up'),
     path("forgot_password/", views.forgot_password, name='forgot_password'),
     path("verify/", views.verify, name="verify"),
